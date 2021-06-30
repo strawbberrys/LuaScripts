dc9877442f1d8f161a9b201da7dccdf54ddd3351
strawberrys <56797207+strawbberrys@users.noreply.github.com>
2021-06-30 08:44:40 -0400

    Added information

--- TableDumper.lua
+++ TableDumper.lua
@@ -1,16 +1,18 @@
+-- You can use this as a stand-alone function or use it from a module, just run DumpTable(Table) and it will return that table converted to a string
+
 local DumpTable; do
     DumpTable = function(Table, IndentAmount)
         local IndentAmount, TableAmount, Amount = IndentAmount or 1, 0, 0
         local Indent = ("    "):rep(IndentAmount)
 
         for Index in pairs(Table) do
-            TableAmount++
+            TableAmount = TableAmount + 1
         end
 
         local TableDump, Ending = {((TableAmount > 0 and "{\n") or "{")}, (((IndentAmount > 1 and TableAmount > 0) and "},\n") or "}")
 
         for Index, Value in pairs(Table) do
-            Amount++
+            Amount = Amount + 1
 
             local FixedValue = ((type(Value) == "string" or type(Value) == "function") and "\"" .. tostring(Value) .. "\"" or tostring(Value))
             local Data = table.concat(((type(Value) ~= "table" and {"[\"" .. tostring(Index) .. "\"] = ", FixedValue}) or {"[\"" .. tostring(Index) .. "\"] = ", DumpTable(Value, IndentAmount + 1)}))
